67490c900511d3e7ebc3299755934f471a8060df
hentachii <pavelmagnum19@gmail.com>
2024-12-08 13:27:58 +0500

    Add files via upload

---
+++ Лабораторная работа 4/Task1.py
@@ -0,0 +1,9 @@
+import json
+
+def task() -> float:
+    with open('input.json') as file:
+        data = json.load(file)
+    total_sum = sum(item["score"] * item["weight"] for item in data)
+    return round(total_sum, 3)
+
+print(task())
